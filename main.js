10aba13869a97b03b42dc57597c64d398d3404a8
Gonzalo Martin Rojas <115350959+gonzalomrojas@users.noreply.github.com>
2023-03-01 13:10:50 -0300

    Add files via upload
    commit de proyecto hasta el dia 01/03

---
+++ main.js
@@ -0,0 +1,9 @@
+// let boton = document.getElementById("botonn");
+// boton.addEventListener("click", () => {
+
+//     let input = document.getElementById("searchInput");
+//     let valor = input.value;
+  
+//     alert("El valor del campo es:"+ valor);
+//     console.log(valor)
+// });
